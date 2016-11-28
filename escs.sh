b5e5917c58da09dd79d2ff3b76ef1f7fdf9590d2
Iury O. G. Figueiredo <ioliveira.id.uff.br>
2016-11-28 18:36:45 -0200

    Adding escs.

---
+++ escs.sh
@@ -0,0 +1,14 @@
+##############################################################################
+# manage, use, haskell, github.
+
+cd ~/projects
+git clone git@github.com:iogf/projecteuler.haskell.git projecteuler.haskell-code
+
+# push, update, haskell.
+cd ~/projects/projecteuler.haskell-code
+git status
+git add *
+git commit -a
+git push
+
+
